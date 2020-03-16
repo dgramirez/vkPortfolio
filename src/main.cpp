37ac8e70aa522eaf0b4b246e16cc8f6e081f385b
Derrick G. Ramirez <dgramirez@protonmail.com>
2020-03-16 10:35:27 -0400

    Linked all necessary Dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include "pch.h"
-#include <stdio.h>
-
 int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+//Additional Implementations provided by single header libraries
+#define STB_IMAGE_IMPLEMENTATION
+#include "../dep/stbimage/stb_image.h"
+#define VMA_IMPLEMENTATION
+#include "../dep/vmaAllocator/vk_mem_alloc.h"
